6a4d9fc79758d742a5d9f45d79ef97fbe186f384
Hridoy421eng <mdhihridoy1@gmailcom>
2026-02-22 21:08:48 +0600

    added the category wise button and modal

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ homeBtn.forEach(btn => {
     const elements = document.getElementsByClassName("hide-active-products");
     for (let el of elements){
         el.style.display = "block";
-    }
+    };
 });
 });
 
@@ -38,8 +38,21 @@ function removeActiveClass(){
     const activeButtons = document.getElementsByClassName("active");
     for(let btn of activeButtons){
         btn.classList.remove("active");
-    }
-}
+        btn.classList.remove("active-bg");
+        
+    };
+};
+
+
+const allBtn = document.getElementById("all-btn");
+
+   allBtn.addEventListener("click", () => {
+    removeActiveClass();
+    allBtn.classList.add("active");
+    allBtn.classList.add("active-bg");
+
+    loadProducts();
+});
 
 
 
@@ -92,7 +105,7 @@ const showTrendings = (products) => {
             </div>
         `
         trendingContainer.appendChild(div)
-    }
+    };
 };
 
 //................  categories are here..........................
@@ -100,40 +113,53 @@ const showTrendings = (products) => {
 const loadCategories = async() =>{
     const response = await fetch("https://fakestoreapi.com/products/categories");
     const data = await response.json();
-    showCategories(data)
+    showCategories(data);
 
-//     function removeActiveClass(){
-//     const activeButtons = document.querySelectorAll(".active");
-//     activeButtons.forEach(btn => btn.classList.remove("active"));
-// }
-}
+};
 
+
+// category showing here........
 
 const showCategories = (categories) => {
 
 
     const categoryContainer = document.getElementById("category-Container");
-        categoryContainer.innerHTML = "";
-
+        // categoryContainer.innerHTML = "";
 
     for (let cat of categories){
         const categoryBtn = document.createElement("div");
         categoryBtn.innerHTML= `
-        <button class="btn rounded-3xl">${cat}</button>
+        <button class="btn category-btns rounded-3xl">${cat}</button>
         `   ;
-        // const button = categoryBtn.querySelector("button");
 
-        // button.addEventListener("click", () => {
-        //     removeActiveClass();
-        //     button.classList.add("active");
-        // });
+        const button = categoryBtn.querySelector(".category-btns");
+
+
+
+        button.addEventListener("click", () => {
+            removeActiveClass();
+            button.classList.add("active");
+            button.classList.add("active-bg");
+
+            document.getElementById("all-product-category").style.display = "block";
+            const elements = document.getElementsByClassName("hide-active-products");
+   for (let el of elements){
+    // console.log(el)
+    
+       el.style.display = "none";
+   }
+   loadCategoryProducts(cat);
+            
+        });
 
              categoryContainer.appendChild(categoryBtn);
 
-    }
+    };
 
-}
+};
 
+
+// // every single products api calling........
 
 const loadProducts = () => {
     const url = "https://fakestoreapi.com/products";
@@ -145,13 +171,54 @@ const loadProducts = () => {
 };
 
 
+//  category wise api callling...........
+
+const loadCategoryProducts = (category) => {
+//    console.log(category) 
+   fetch(`https://fakestoreapi.com/products/category/${category}`)
+   .then(res => res.json())
+   .then(data => showProducts(data));
+
+};
+
+
+// details modal here.........
+
+const loadProductDetails = (productId) => {
+       console.log(productId);
+       fetch(`https://fakestoreapi.com/products/${productId}`)
+       .then(res => res.json())
+       .then(data => displayProductDetails(data));
+};
+
+const displayProductDetails = (product) => {
+     console.log(product);
+     document.getElementById("product_details").showModal();
+
+     const detailsContainer = document.getElementById("details-container");
+     detailsContainer.innerHTML = `
+     <h2 class="font-bold">${product.title}</h2>
+     <p class="mt-2">${product.description}</p>
+     <div class="mt-3">
+         <span class="font-semibold mt-2 mr-5">Price: $${product.price}</span>Rating:
+         <span><i class="text-yellow-300 fa-regular fa-star mr-1"></i></span>
+         <span class="font-semibold">${product.rating.rate}</span></div>
+
+     `
+};
+
+
+// every single products show card........
+
 const showProducts = (products) => {
      
     const allProductsContainer = document.getElementById("all-product-Container");
     allProductsContainer.innerHTML ="";
-    for (let product  of products) {
 
-        const div = document.createElement("div");
+            
+            for ( let product of products) {
+              
+                const div = document.createElement("div");
         div.innerHTML = `   
            <div class="card bg-base-100 w-96 shadow-sm flex items-center justify-center rounded-xl">
                        <div class="w-full h-96 bg-slate-200 items-center justify-center">
@@ -173,23 +240,31 @@ const showProducts = (products) => {
                                  <p class="mt-2 font-bold">${product.title}</p>
                                  <p class="text-xl font-bold mt-2">$${product.price}</p>
                      <div class="card-actions justify-between mt-3">
-                        <button type="submit" class="btn w-40"><span><i class="fa-regular fa-eye"></i></span>Details</button>
+                        <button onclick="loadProductDetails(${product.id})" type="submit" class="btn w-40"><span><i class="fa-regular fa-eye"></i></span>Details</button>
                         <button class="btn btn-primary w-40"><span><i class="fa-solid fa-cart-shopping"></i></span>Add</button>
                      </div>
                   </span>
-            </div>
-        `
-        allProductsContainer.appendChild(div)
-    }
+            </div>`
+            allProductsContainer.appendChild(div);
+
+            };   
+          
 };
 
 // product button functionality..........
 
+// function categoryAllBtnClicked() {
+//     console.log('I am clicked all button')
+// }
+
+
+
+// nav products button functionality...........
 
 productBtn.forEach(btn =>{
     btn.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log('Products button clicked')
+   
 
 
   document.getElementById("all-product-category").style.display = "block";
@@ -199,7 +274,8 @@ productBtn.forEach(btn =>{
     console.log(el)
     
        el.style.display = "none";
-   }
+   };
+
 });
 });
 
@@ -216,16 +292,5 @@ loadProducts()
 
 
 
-// {
-// "id": 5,
-// "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
-// "price": 695,
-// "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-// "category": "jewelery",
-// "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_t.png",
-// "rating": {
-// "rate": 4.6,
-// "count": 400
-// }
-// }
+
 
